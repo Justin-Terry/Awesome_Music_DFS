e52060013d4b9ac0b51cfc1b27925af96e8204a2
Unknown <c_wance@msn.com>
2019-10-29 11:02:43 -0700

    a

--- Awesome_Music_DFS/src/chord/DFSCommand.java
+++ Awesome_Music_DFS/src/chord/DFSCommand.java
@@ -5,6 +5,8 @@ import java.io.*;
 import com.google.gson.*;
 import com.google.gson.stream.*;
 
+import models.Record;
+
 
 
 
@@ -52,11 +54,11 @@ public class DFSCommand
     static public void main(String args[]) throws Exception
     {
         Gson gson = new Gson();
-        RemoteInputFileStream in = new RemoteInputFileStream("music.json", false);
+        RemoteInputFileStream in = new RemoteInputFileStream("./src/resources/music.json", false);
         in.connect();
         Reader targetReader = new InputStreamReader(in);
         JsonReader jreader = new  JsonReader(targetReader);
-        Music[] music = gson.fromJson(jreader, Music[].class);
+        Record[] music = gson.fromJson(jreader, Record[].class);
         
         if (args.length < 1 ) {
             throw new IllegalArgumentException("Parameter: <port> <portToJoin>");
